1de33d3924560bf96d9dfa48a576dbcc1aca1f16
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-06-01 16:00:51 -0300

    Initial commit

---
+++ Assets/Scripts/gunScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunScript : MonoBehaviour
+{
+    public GameObject bullet, bulletSpawnPoint;
+
+    public float bulSpeed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Click To Shoot
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //Spawns Bullets
+            GameObject bulClone = Instantiate(bullet);
+            bulClone.transform.position = bulletSpawnPoint.transform.position;
+
+            bulClone.GetComponent<Rigidbody>().AddForce(transform.forward * bulSpeed);
+        }
+
+    }
+}
