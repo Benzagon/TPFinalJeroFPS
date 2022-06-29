2735a8b1fb6fd1881e998757577001613a8cab2b
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-06-29 10:04:13 -0300

    CannonBall Fire Implemented

---
+++ Assets/Scripts/cannonFireScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonFireScript : MonoBehaviour
+{
+    public GameObject cannonBall, ballSpawnPoint;
+
+    public float ballSpeed;
+    public float ballDestroyTime;
+    public float cannonFireDelay;
+
+    float spawnBallTimer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        spawnBallTimer += Time.deltaTime;
+        if(spawnBallTimer >= cannonFireDelay)
+        {
+            fireBall();
+            spawnBallTimer = 0f;
+        }
+    }
+
+    void fireBall()
+    {
+        //Spawns Cannon Balls
+        GameObject bulClone = Instantiate(cannonBall);
+        bulClone.transform.position = ballSpawnPoint.transform.position;
+
+        bulClone.GetComponent<Rigidbody>().AddForce(transform.right * ballSpeed);
+
+        Destroy(bulClone, ballDestroyTime);
+    }
+}
